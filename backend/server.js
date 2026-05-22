0e449f1557c15d7d517b9d912544ff310ec9b6c8
Aleksandr Lavrik <lexxlar05@gmail.com>
2026-05-22 08:59:47 +0300

    Обновление конфигурации сервера, обновление методов прогрессии

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/professions', require('./routes/professions'));
 app.use('/api/quiz', require('./routes/quiz'));
+app.use('/api/professions', require('./routes/professions'));
+
+app.use('/api/progress', require('./routes/progress'));
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Сервер запущен на http://localhost:${PORT}`));
